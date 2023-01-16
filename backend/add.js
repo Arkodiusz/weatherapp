9849bcc96320144dd19ee1df6ed7b1ed1cd473af
Arkodiusz <arecki358@gmail.com>
2023-01-16 20:43:57 +0100

    api integrated with lambda - passing url params to 'add' lambda

--- backend/add.js
+++ backend/add.js
@@ -1,9 +1,13 @@
-var aws = require('aws-sdk');
-var dynamodb = new aws.DynamoDB();
-var tableName = process.env.TABLE_NAME ;
+var aws = require('aws-sdk')
+var dynamodb = new aws.DynamoDB()
+var tableName = process.env.TABLE_NAME
 
-exports.handler = (event, context, callback) => {
-    console.log(event)
-    console.log(context)
-    console.log(callback)
-};
+exports.handler =  async function(event, context) {
+    var name = event.queryParams.name
+    var long = event.queryParams.long
+    var lat = event.queryParams.lat
+
+    console.log('name', name)
+    console.log('long', long)
+    console.log('lat', lat)
+}
--- frontend/scripts.js
+++ frontend/scripts.js
@@ -1,35 +1,60 @@
-// Using good-old jQuery to fill the table with data received from the API Gateway endpoint
+const API_URL = 'https://r5tlx9s6u5.execute-api.eu-central-1.amazonaws.com/prod'
 
-header = `<tr> \
-            <th>Name</th> \
-            <th>Longitude</th> \
-            <th>Latitude</th> \
-            <th>Temperature</th> \
-        </tr>`
+const API_URL_SCAN = API_URL + '/scan'
+const API_URL_ADD = API_URL + '/add'
 
-function draw_table(rows) {
-    $('table.table').append(header)
-    $('table.table').append(rows.join())
-    $('#loading').hide()
-    $('table.table').show()
+const htmlHeaderRow =
+`<tr>
+    <th>Name</th>
+    <th>Longitude</th>
+    <th>Latitude</th>
+    <th>Temperature</th>
+    <th></th>
+</tr>`
+
+const htmlFormRow=
+`<tr><td></td></tr><tr>
+    <td><input type="text" id="form_name" placeholder="Name" style='width:100%' /></td>
+    <td><input type="number" id="form_long" placeholder="[-180..180]" min="-180" max="180" style='width:100%' /></td>
+    <td><input type="number" id="form_lat" placeholder="[-90..90]" min="-90" max="90" style='width:100%' /></td>
+    <td></td>
+    <td><button onclick="addItem()" style='width:100%'>Save</button></td> \
+</tr>`
+
+function addItem() {
+    var name = $('#form_name').val()
+    var long = $('#form_long').val()
+    var lat = $('#form_lat').val()
+
+    var path = API_URL_ADD + '?name=' + name + '&long=' + long + '&lat=' + lat
+
+     $.get(path, function() {
+        console.log(path)
+     })
+
+    refresh()
+//    $('#form_name').val(null)
+//    $('#form_long').val(null)
+//    $('#form_lat').val(null)
 }
 
-function hide_table() {
-    $('table.table').empty();
-    $('#loading').show();
+function draw_table(rows) {
+    $('#table1').empty();
+    $('#table1').append(htmlHeaderRow)
+    $('#table1').append(rows.join())
+    $('#table1').append(htmlFormRow)
+    $('#loading').hide()
+    $('#table1').show()
 }
 
 function refresh() {
-    hide_table();
-
-    var api_gateway_url = 'https://8kz5g7t6if.execute-api.eu-central-1.amazonaws.com/prod/scan';
     var rows = [];
 
-    $.get(api_gateway_url, function(data) {
+    $.get(API_URL_SCAN, function(data) {
         data.forEach(function(item) {
             console.log('item', item)
 
-            name =  item['PriKey']['S']
+            name =  item['name']['S']
             long = item['longitude']['N']
             lat = item['latitude']['N']
             temp = 0
